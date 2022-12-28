08fce25199175f6cfea8c55e160e07a585c0525e
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-12-28 23:44:41 +0900

    commit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 19:00:00 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/28 23:35:18 by yokitaga         ###   ########.fr       */
+/*   Updated: 2022/12/28 23:44:25 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ int open_map(int fd, int argc, char argv[])
     return (fd);
 }
 
+void init_map(map_data_t map_data)
+{
+    map_data->game_map = NULL;
+    map_data->height = 0;
+    map_data->width = 0;
+    map_data->nbr_of_enemy = 0;
+    map_data->nbr_of_collectable = 0;
+    map_data->nbr_of_exit = 0;
+    map_data->nbr_of_player = 0;
+}
+
 void read_map(int fd, map_data_t  map_data)
 {
     char *read_line;
@@ -33,9 +44,12 @@ void read_map(int fd, map_data_t  map_data)
     {
         read_line = get_next_line(fd);
         map_data->game_map[line] = read_line;
+        free(read_line);
         line++;
     }
+    if ()
 }
+
 void map_check(char *game_map[])
 {
        
@@ -47,6 +61,7 @@ int main(int argc, char *argv[])
     map_data_t  map_data;
 
     fd = open_map(fd, argc, argv);
+    init_map(map_data);
     read_map(fd, map_data);
     map_check(map_data->game_map);
     return(0);
