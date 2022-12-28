0fe9774f287251976dd31ad5511160c4717c3795
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-12-28 23:37:36 +0900

    commit

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 19:00:11 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/28 19:41:09 by yokitaga         ###   ########.fr       */
+/*   Updated: 2022/12/28 23:34:22 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-# define WRONG_ARGC 1
-# define OPEN_ERROR 2
+# define MAX_LINES      100000
+# define WRONG_ARGC     1
+# define OPEN_ERROR     2
+# defien INVARID_MAP    3 
 
-typedef struct map_info{
-    int nbr_of_enemy;
-    int nbr_of_collectable;
-    int nbr_of_exit;
-    int nbr_of_player;
-}map_info_copy
+typedef struct map_datas{
+    char    *game_map[MAX_LINES];
+    int     height;
+    int     width;
+    int     nbr_of_enemy;
+    int     nbr_of_collectable;
+    int     nbr_of_exit;
+    int     nbr_of_player;
+}map_data_t;
+
+typedef struct game_datas{
+    map_data_t  map_data;
+}game_data_t;
+
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 19:00:00 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/28 19:37:31 by yokitaga         ###   ########.fr       */
+/*   Updated: 2022/12/28 23:35:18 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include "../gnl/get_next_line.h"
 
 int open_map(int fd, int argc, char argv[])
 {
@@ -22,11 +23,31 @@ int open_map(int fd, int argc, char argv[])
     return (fd);
 }
 
+void read_map(int fd, map_data_t  map_data)
+{
+    char *read_line;
+    int  line;
+
+    line = 0;
+    while (read_map != NULL)
+    {
+        read_line = get_next_line(fd);
+        map_data->game_map[line] = read_line;
+        line++;
+    }
+}
+void map_check(char *game_map[])
+{
+       
+}
+
 int main(int argc, char *argv[])
 {
-    static int fd;
-    
+    static int  fd;
+    map_data_t  map_data;
+
     fd = open_map(fd, argc, argv);
-    
+    read_map(fd, map_data);
+    map_check(map_data->game_map);
     return(0);
 }
