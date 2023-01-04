484ad3b2608508331a3c9ac322d70a1a7a1fa8f3
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-04 12:02:03 +0900

    commit

--- V2/srcs/3_parse_map.c
+++ V2/srcs/3_parse_map.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 23:28:11 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/04 11:39:55 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/04 12:01:53 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ void parse_map(t_data *data)
 {
     check_line(data);
     check_column(data);
-    count_parm(data);
+    count_inside(data);
     check_parm(data);
+    check_position(data);
 }
 
 void check_line(t_data *data)
@@ -51,12 +52,63 @@ void check_column(t_data *data)
     }
 }
 
-void count_parm(t_data *data)
+void check_inside(t_data *data)
 {
+    size_t x;
+    size_t y;
     
+    y = 0;
+    while (y < data->map.height)
+    {
+        x = 0;
+        while (x < data->map.width)
+        {
+            if (data->map.map[y][x] == PLAYER)
+            {
+                data->map.n_player++;
+                data->map.player.x = x;
+                data->map.player.y = y;
+            }
+            else if (data->map.map[y][x] == COLLECTIBLE)
+                data->map.n_collectibel++;
+            else if (data->map.map[y][x] == EXIT)
+                data->map.n_exit++;
+            else if (ft_strchr("C01PE", data->map.map[y][x]) == NULL)
+                put_error_and_exit("INVALID ERROR", data);
+            x++;
+        }
+        y++;
+    }
 }
 
 void check_parm(t_data *data)
 {
-    
+    if (data->map.n_collectibel == 0)
+        put_error_and_exit("INVALID ERROR", data);
+    else if (data->map.n_exit != 1)
+        put_error_and_exit("INVALID ERROR", data);
+    else if (data->map.n_player != 1)
+        put_error_and_exit("INVALID ERROR", data);
+}
+
+void check_position(t_data *data)
+{
+    size_t x;
+    size_t y;
+
+    y = 0;
+    while (y < data->map.height)
+    {
+        x = 0;
+        while (x < data->map.width)
+        {
+            if (data->map.map[y][x] == COLLECTIBLE || data->map.map[y][x] == EXIT || data->map.map[y][x] == PLAYER)
+            {
+                if (data->map.map[y-1][x-1] == WALL && data->map.map[y-1][x] == WALL && data->map.map[y-1][x+1] == WALL && data->map.map[y][x-1] == WALL && data->map.map[y][x+1] == WALL && && data->map.map[y+1][x-1] == WALL && && data->map.map[y+1][x] == WALL && && data->map.map[y+1][x+1] == WALL)
+                    put_error_and_exit("INVALID ERROR", data);
+            }
+            x++;
+        }
+        y++;
+    }
 }
