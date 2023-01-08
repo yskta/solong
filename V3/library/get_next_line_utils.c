09b73a6baf2e75eb9c2483f3c1b05dff1d7f8629
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-08 22:43:47 +0900

    commit

---
+++ V3/library/get_next_line_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 11:18:49 by yokitaga          #+#    #+#             */
+/*   Updated: 2022/12/15 21:54:26 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*p;
+	size_t	i;
+	size_t	j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	p = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (p == NULL)
+		return (ft_free(s1));
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+		p[i++] = s2[j++];
+	p[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (p);
+}
+
+char *ft_free(char *str)
+{
+	free(str);
+	return(NULL);
+}
