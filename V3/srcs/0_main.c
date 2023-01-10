2234ef6a45b3d28c92af5960d2b4d24de9db2f3a
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-10 22:22:08 +0900

    commit

--- V3/srcs/0_main.c
+++ V3/srcs/0_main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 21:46:33 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/08 23:16:01 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/10 22:21:58 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
     parse_map(data);
     init_other_data(data);
     render_map(data);
-    //mlx_hook(data->mlx_win, 9, 1L << 21, close_game, data);
-    //mlx_key_hook(data->mlx_win, key_action, data);
-    //mlx_loop_hook(data->mlx, render_map, data);
-    //mlx_loop(data->mlx);
+    mlx_hook(data->mlx_win, 9, 1L << 21, close_game, data);
+    mlx_key_hook(data->mlx_win, key_action, data);
+    mlx_loop_hook(data->mlx, render_map, data);
+    mlx_loop(data->mlx);
     return (0);
 }
