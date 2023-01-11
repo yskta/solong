217415ba3748884a9fe1594d2ee27bd9b6227fee
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-11 21:23:38 +0900

    commit

--- V3/srcs/6_render_map.c
+++ V3/srcs/6_render_map.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 21:50:30 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/11 20:02:00 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/11 21:23:28 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,13 @@ int render_map(t_data *data)
 void id_and_put_image(t_data *data, size_t y, size_t x)
 {
     if (data->map.map[y][x] == COLLECTIBLE)
-        mlx_put_image_to_window (data->mlx, data->mlx_win, data->collectible.img_ptr, x, y);
+        mlx_put_image_to_window (data->mlx, data->mlx_win, data->collectible.img_ptr, x * data->collectible.x, y * data->collectible.y);
     else if (data->map.map[y][x] == SPACE)
-        mlx_put_image_to_window (data->mlx, data->mlx_win, data->space.img_ptr, x, y);
+        mlx_put_image_to_window (data->mlx, data->mlx_win, data->space.img_ptr, x * data->space.x , y * data->space.y);
     else if (data->map.map[y][x] == WALL)
-        mlx_put_image_to_window (data->mlx, data->mlx_win, data->wall.img_ptr, x, y);
+        mlx_put_image_to_window (data->mlx, data->mlx_win, data->wall.img_ptr, x * data->wall.x , y * data->wall.y);
     else if (data->map.map[y][x] == EXIT)
-        mlx_put_image_to_window (data->mlx, data->mlx_win, data->exit.img_ptr, x, y);
+        mlx_put_image_to_window (data->mlx, data->mlx_win, data->exit.img_ptr, x * data->exit.x , y * data->exit.y);
     else if (data->map.map[y][x] == PLAYER)
-        mlx_put_image_to_window (data->mlx, data->mlx_win, data->player.img_ptr, x, y);
+        mlx_put_image_to_window (data->mlx, data->mlx_win, data->player.img_ptr, x * data->player.x, y * data->player.y);
 }
