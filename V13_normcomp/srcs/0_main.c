65a8cb391c3c63fdc11562eefa45843e5919b14c
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2023-01-16 00:50:01 +0900

    commit

--- V13_normcomp/srcs/0_main.c
+++ V13_normcomp/srcs/0_main.c
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 21:46:33 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/01/15 17:47:25 by yokitaga         ###   ########.fr       */
+/*   Updated: 2023/01/16 00:48:48 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int	main(int argc, char *argv[])
 	parse_map(data);
 	init_other_data(data);
 	render_map(data);
-	mlx_hook(data->mlx_win, Expose, ExposureMask, render_map, data);
+	mlx_hook(data->mlx_win, FocusIn, FocusChangeMask, render_map, data);
 	mlx_hook(data->mlx_win, KeyPress, KeyPressMask, key_action, data);
-	mlx_hook(data->mlx_win, DestroyNotify, ButtonPressMask, close_game, data);
+	mlx_hook(data->mlx_win, DestroyNotify, StructureNotifyMask, close_game, data);
 	mlx_loop(data->mlx);
 	return (0);
 }
